3dec17776e886ef1a997d12edf664df5e48fba78
guilevieiram <guilhermevmanhaes@gmail.com>
2023-04-06 08:54:12 +0200

    72% on unseen data

--- whatIs/config.py
+++ whatIs/config.py
@@ -4,8 +4,8 @@ NUM_SAMPLES = 1000
 
 # dataset
 
-DATASET_SIZE = 100_000
-# DATASET_SIZE = 1
+# DATASET_SIZE = 100_000
+DATASET_SIZE = 1
 
 PROMPT_SIZE = 6
 
@@ -23,7 +23,7 @@ LOSS_THRESHOLD = 5e-5
 
 # detection
 class Detection: 
-    NUM_EPOCHS = 15
+    NUM_EPOCHS = 30
     LEARNING_RATE = 1e-4
     NUM_QUERIES = 10
     WEIGHT_DECAY = 1e-3
