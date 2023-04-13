bebd3c24202ed203adde9dc0f6d8417c4f22a2c3
guilevieiram <guilhermevmanhaes@gmail.com>
2023-04-13 18:37:58 +0200

    detection with batches

--- whatIs/config.py
+++ whatIs/config.py
@@ -23,8 +23,9 @@ LOSS_THRESHOLD = 5e-5
 
 # detection
 class Detection: 
-    NUM_EPOCHS = 30
+    NUM_EPOCHS = 15
     LEARNING_RATE = 1e-4
     NUM_QUERIES = 10
     WEIGHT_DECAY = 1e-3
     LAMBDA_L1 = 1e-4
+    BATCH_SIZE = 64
