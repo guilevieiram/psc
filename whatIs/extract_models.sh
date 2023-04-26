05b573e657eb4ae1c3f5277e510d402d15cbd285
guile <guile@flyskyler.com>
2023-04-26 15:30:53 +0200

    update notebooks

--- whatIs/extract_models.sh
+++ whatIs/extract_models.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 
+
 echo Extracting models
-echo Count before: 
-ls ./psc/finals | wc -l;
 
 cd ~;
 
@@ -13,17 +12,8 @@ cd pscdata;
 scp -r guilherme.vieira-manhaes@vanda.polytechnique.fr:~/*.fr .;
 
 mkdir finals results;
-mmv "*.fr/psc/whatIs/finals/*.pkl" "./finals/#2_05042023_v3_#1.pkl";
-mmv "*.fr/psc/whatIs/results.csv" "./results/results_#1_05042023_v3.csv";
-
-
-cd ~;
-cp -r ./pscdata/finals/*.pkl ./psc/finals/;
-cp -r ./pscdata/results/*.csv ./psc/finals/;
-
-
-echo Count after: 
-ls ./psc/finals | wc -l;
+mmv "*.fr/psc/whatIs/finals/*.pkl" "./finals/#2_17042023_#1.pkl";
+mmv "*.fr/psc/whatIs/results.csv" "./results/results_#1_17042023.csv";
 
 
 echo Finished extraction!
